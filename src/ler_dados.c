ce29a5a1180be2c908a6a6293b0efcd170593173
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2026-07-14 12:59:15 -0300

    feat: entendendo lógica de leitura de dados, inicial

---
+++ src/ler_dados.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char data_compra[9];
+    char cod_cliente[9];
+    char cod_produto[6];
+    char nome_produto[100];
+} RegistroCompra;
+
+int main() {
+    FILE *arquivo;
+    RegistroCompra registro;
+
+    // Abre arquivo CSV para leitura
+    //TODO: loop para abrir todos os arquivos de dados
+    arquivo = fopen("data/dados_venda_cluster_0.csv", "r");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    // Ignora a primeira linha (cabeçalho)
+    fscanf(arquivo, "%*[^\n]\n");
+
+    // Lê cada linha do arquivo CSV
+    while (fscanf(arquivo, "%8[^;];%8[^;];%5[^;];%99[^\n]\n",
+        registro.data_compra, 
+        registro.cod_cliente, 
+        registro.cod_produto, 
+        registro.nome_produto) == 4) {
+        // Imprime os dados lidos
+        printf("%s | %s | %s | %s\n", 
+            registro.data_compra, 
+            registro.cod_cliente, 
+            registro.cod_produto, 
+            registro.nome_produto);
+    }
+
+    // Fecha o arquivo
+    fclose(arquivo);
+    return 0;
+}
