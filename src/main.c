0e61c10882e31b013df17d160dc88e0183264ba4
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2026-07-22 10:29:07 -0300

    refactor: voltando o código da atividade 1 do "zero"

--- src/main.c
+++ src/main.c
@@ -5,30 +5,5 @@
 #include "../include/lista_compras.h"
 
 int main(void) {
-    DadosCompras dados;
-    ListaCompras *lista;
-    char codigoCliente[9];
-
-    dados = lerArquivo("data/dados_venda_cluster_0.csv");
-
-    if (dados.registros == NULL) {
-        return 1;
-    }
-
-    lista = criaListaCompras(dados);
-
-    for (int i = 0; i < 3; i++) {
-        printf("\nDigite o codigo do cliente: ");
-        scanf("%8s", codigoCliente);
-
-        exibeComprasCliente(
-            lista,
-            codigoCliente
-        );
-    }
-
-    liberaListaCompras(lista);
-    free(dados.registros);
-
     return 0;
 }
