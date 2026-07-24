8cc82c4ebb5832682b055117949bd5e6eec856b1
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2026-07-24 14:35:41 -0300

    refactor: otimização, a matriz transposta não é mais alocada na memória

--- src/similaridade.cpp
+++ src/similaridade.cpp
@@ -30,39 +30,58 @@ void geraMatrizCompras(Similaridade *similaridade, const ListaCompras *lista_com
     }
 }
 
-Matriz getTransposta(Matriz a, int linhas, int colunas) {
-    Matriz transposta = (Matriz) malloc(colunas * sizeof(int *)); // todo: otimizar alocação da matriz transposta
-    for (int j = 0; j < colunas; j++) {
-        transposta[j] = (int *) calloc(linhas, sizeof(int));
+// Matriz getTransposta(Matriz a, int linhas, int colunas) {
+//     Matriz transposta = (Matriz) malloc(colunas * sizeof(int *)); // todo: otimizar alocação da matriz transposta
+//     for (int j = 0; j < colunas; j++) {
+//         transposta[j] = (int *) calloc(linhas, sizeof(int));
+//     }
+
+//     for (int i = 0; i < linhas; i++)
+//         for (int j = 0; j < colunas; j++)
+//             transposta[j][i] = a[i][j];
+//     return transposta;
+// }
+
+// Matriz getProdutoMatrizes(Matriz a, int linhas_a, int colunas_a, Matriz b, int colunas_b) {
+//     Matriz matriz_interesecao = (Matriz) malloc(linhas_a * sizeof(int *));
+//     for (int i = 0; i < linhas_a; i++) {
+//         matriz_interesecao[i] = (int *) calloc(colunas_b, sizeof(int));
+//     }
+
+//     for (int i = 0; i < linhas_a; i++)
+//         for (int j = 0; j < colunas_b; j++)
+//             for (int k = 0; k < colunas_a; k++)
+//                 matriz_interesecao[i][j] += a[i][k] * b[k][j];
+//     return matriz_interesecao;
+// }
+
+Matriz getMatrizIntersecao(Matriz matriz_compras, int quantidade_clientes, int quantidade_produtos) {
+    Matriz matriz_intersecao = (Matriz) malloc(quantidade_clientes * sizeof(int *));
+
+    for (int i = 0; i < quantidade_clientes; i++) {
+        matriz_intersecao[i] = (int *) calloc(quantidade_clientes, sizeof(int));
     }
 
-    for (int i = 0; i < linhas; i++)
-        for (int j = 0; j < colunas; j++)
-            transposta[j][i] = a[i][j];
-    return transposta;
-}
+    for (int i = 0; i < quantidade_clientes; i++) {
+        for (int j = i; j < quantidade_clientes; j++) {
+            int intersecao = 0;
 
-Matriz getProdutoMatrizes(Matriz a, int linhas_a, int colunas_a, Matriz b, int colunas_b) {
-    Matriz matriz_interesecao = (Matriz) malloc(linhas_a * sizeof(int *));
-    for (int i = 0; i < linhas_a; i++) {
-        matriz_interesecao[i] = (int *) calloc(colunas_b, sizeof(int));
+            for (int k = 0; k < quantidade_produtos; k++) {
+                intersecao += matriz_compras[i][k] * matriz_compras[j][k];
+            }
+
+            matriz_intersecao[i][j] = intersecao;
+            matriz_intersecao[j][i] = intersecao;
+        }
     }
 
-    for (int i = 0; i < linhas_a; i++)
-        for (int j = 0; j < colunas_b; j++)
-            for (int k = 0; k < colunas_a; k++)
-                matriz_interesecao[i][j] += a[i][k] * b[k][j];
-    return matriz_interesecao;
+    return matriz_intersecao;
 }
 
 void calculaMatrizSimilaridade(Similaridade *similaridade, const ListaCompras *lista_compras) {
     geraMatrizCompras(similaridade, lista_compras);
 
-    Matriz transposta = getTransposta(similaridade->matriz_compras, similaridade->linha_matriz, similaridade->coluna_matriz);
-    similaridade->matriz_intersecao = getProdutoMatrizes(similaridade->matriz_compras, similaridade->linha_matriz, similaridade->coluna_matriz, transposta, similaridade->linha_matriz);
-
-    for (int i = 0; i < similaridade->coluna_matriz; i++) free(transposta[i]);
-    free(transposta);
+    similaridade->matriz_intersecao = getMatrizIntersecao(similaridade->matriz_compras, similaridade->linha_matriz, similaridade->coluna_matriz);
 
     similaridade->matriz_similaridade = (MatrizDouble) malloc(similaridade->linha_matriz * sizeof(double *));
     for (int i = 0; i < similaridade->linha_matriz; i++) {
