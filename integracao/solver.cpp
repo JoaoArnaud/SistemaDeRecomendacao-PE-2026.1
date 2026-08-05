4b2b4bbaf0a6f5428097233e4c12d26b674239a3
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2026-08-04 22:42:24 -0300

    feat: criando primeiros comandos básicos no pybind11

--- integracao/solver.cpp
+++ integracao/solver.cpp
@@ -1,5 +1,15 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include "../include/lista_compras.h"
+#include "../include/similaridade.h"
+#include "../include/recomendacao.h"
+
 namespace py = pybind11;
 
+PYBIND11_MODULE(solver, m) {
+    m.doc() = "Módulo de integração para solver";
+
+    m.def("calcular_similaridade", &calcular_similaridade, "Calcula a similaridade entre clientes com base em suas listas de compras");
+    m.def("gerar_recomendacoes", &gerar_recomendacoes, "Gera recomendações para um cliente com base KNN");
+}
